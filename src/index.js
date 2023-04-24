c66178886a667c870ec8b724ec8dface5b6f0432
Abdurrahman Afsa <23615796+AbdurrahmanAfsa@users.noreply.github.com>
2023-04-24 11:10:28 +0200

    Add an event listener on home button that changes main's inner text to the home text.

--- src/index.js
+++ src/index.js
@@ -9,3 +9,17 @@ pageLoad.nav.menu.addEventListener('click' , () => {
 pageLoad.nav.about.addEventListener('click' , () => {
     about();
 })
+
+pageLoad.nav.home.addEventListener('click' , () => {
+    const main = document.querySelector('main');
+    main.innerText = '';
+    const title = document.createElement('h1');
+    title.classList.add('title');
+    main.appendChild(title);
+    title.innerText = `El-Mi3alem Restaurant`
+
+    const welcome = document.createElement('p');
+    welcome.classList.add('welcome');
+    main.appendChild(welcome);
+    welcome.innerText = `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos cupiditate cumque vel, asperiores debitis numquam non incidunt vitae, ullam accusamus, fuga ratione ducimus quis impedit cum illum. Necessitatibus, illo reprehenderit.`
+})
