1d0c786cd245d69b3a72dd7ae09990635eea385f
Abdurrahman Afsa <23615796+AbdurrahmanAfsa@users.noreply.github.com>
2023-04-24 11:03:39 +0200

    Add about function:     it changes main's inner text to about text. Export about function. Import style.css

---
+++ src/about.js
@@ -0,0 +1,7 @@
+export {about};
+import './style.css';
+
+const about = () => {
+    const main = document.querySelector('main');
+    main.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem deleniti alias dolorem minus, culpa obcaecati quam harum eos laudantium maiores quis aliquam, nobis sunt, voluptas veniam. Debitis non, numquam asperiores doloremque dolor at pariatur magni nostrum praesentium vel voluptatem dolorum exercitationem voluptas explicabo perspiciatis quia fugiat natus excepturi? Possimus, explicabo?';
+}
