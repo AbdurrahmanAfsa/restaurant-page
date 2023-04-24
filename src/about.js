2ef6786dce6b37cf0b291439e890789575ea3de4
Abdurrahman Afsa <23615796+AbdurrahmanAfsa@users.noreply.github.com>
2023-04-24 11:24:53 +0200

    Reset main's inner text Add aboutContainer and append it to main Add aboutWelcome and Para and append them to aboutContainer. Edit aboutWelcome and aboutPara's inner texts.

--- src/about.js
+++ src/about.js
@@ -3,5 +3,18 @@ import './style.css';
 
 const about = () => {
     const main = document.querySelector('main');
-    main.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem deleniti alias dolorem minus, culpa obcaecati quam harum eos laudantium maiores quis aliquam, nobis sunt, voluptas veniam. Debitis non, numquam asperiores doloremque dolor at pariatur magni nostrum praesentium vel voluptatem dolorum exercitationem voluptas explicabo perspiciatis quia fugiat natus excepturi? Possimus, explicabo?';
+    main.innerText = ''
+    const aboutContainer = document.createElement('div');
+    aboutContainer.classList.add('about-container');
+    main.appendChild(aboutContainer);
+
+    const aboutWelcome = document.createElement('h1');
+    aboutWelcome.classList.add('about-welcome');
+    aboutContainer.appendChild(aboutWelcome);
+    aboutWelcome.innerText = 'El-Mi3alem Restaurant';
+
+    const aboutPara = document.createElement('p');
+    aboutPara.classList.add('about-para');
+    aboutContainer.appendChild(aboutPara)
+    aboutPara.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem deleniti alias dolorem minus, culpa obcaecati quam harum eos laudantium maiores quis aliquam, nobis sunt, voluptas veniam. Debitis non, numquam asperiores doloremque dolor at pariatur magni nostrum praesentium vel voluptatem dolorum exercitationem voluptas explicabo perspiciatis quia fugiat natus excepturi? Possimus, explicabo?';
 }
